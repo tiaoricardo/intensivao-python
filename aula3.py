400c92d60cf3234051900c963b651efe5903edb0
Professor Sebastião <tiaoricardo@gmail.com>
2023-01-11 22:42:25 -0300

    web scrapes with selenium

--- aula3.py
+++ aula3.py
@@ -1,3 +1,27 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import pandas as pd
 
 browser = webdriver.Chrome()
+
+browser.get('https://www.google.com/')
+
+xinput = '/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input' 
+xdolar = '//*[@id="knowledge-currency__updatable-data-column"]/div[1]/div[2]/span[1]'
+
+browser.find_element('xpath',xinput).send_keys('cotação dolar')
+browser.find_element('xpath',xinput).send_keys(Keys.ENTER)
+
+quotation = browser.find_element('xpath',xdolar).get_attribute('data-value')
+
+print(quotation)
+
+df = pd.read_excel('Produtos.xlsx')
+
+df.loc[df["Moeda"] == 'Dólar', 'Cotação'] = float(quotation)
+df['Preço de Compra'] = df['Cotação'] * df['Preço Original']
+df['Preço de Venda'] = df['Preço de Compra'] * df['Margem']
+
+df.to_excel('Produtos_novo.xlsx', index=False)
+
+browser.quit()
